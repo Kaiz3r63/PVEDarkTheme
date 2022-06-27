53b18b4d413f8d360af209b41d042fb2f35aa47a
Kaiz3r63 <36544602+Kaiz3r63@users.noreply.github.com>
2022-06-27 10:14:25 +0200

    Traduction du code

--- PVEDiscordDark.sh
+++ PVEDiscordDark.sh
@@ -132,7 +132,7 @@ function status {
         if isInstalled; then
             echo -e "  Status:      ${GRN}present${REG}"
         else
-            echo -e "  Status:      ${RED}not present${REG}"
+            echo -e "  Status:      ${RED}non present${REG}"
         fi
         echo -e "  CSS:         $(sha256sum /usr/share/pve-manager/css/dd_style.css 2>/dev/null  || echo N/A)"
         echo -e "  JS:          $(sha256sum /usr/share/pve-manager/js/dd_patcher.js 2>/dev/null  || echo N/A)\n"
@@ -146,15 +146,15 @@ function status {
 
 function install {
     if isInstalled; then
-        if [ "$_silent" = false ]; then echo -e "${RED}Theme already installed${REG}"; fi
+        if [ "$_silent" = false ]; then echo -e "${RED}Thème déja installé${REG}"; fi
         exit 2
     else
         if [ "$_silent" = false ]; then checkSupported; fi
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Backing up template file"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Sauvegarde du fichier de template"; fi
         cp $TEMPLATE_FILE $TEMPLATE_FILE.bak
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Downloading stylesheet"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Téléchargement de la feuille de style"; fi
 
         if [ "$OFFLINE" = false ]; then
             curl -s $BASE_URL/PVEDiscordDark/sass/PVEDiscordDark.css > /usr/share/pve-manager/css/dd_style.css
@@ -169,7 +169,7 @@ function install {
             cp "$OFFLINEDIR/PVEDiscordDark/js/PVEDiscordDark.js" /usr/share/pve-manager/js/dd_patcher.js
         fi
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Applying changes to template file"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Application des changements au fichier de template"; fi
         if !(grep -Fq "<link rel='stylesheet' type='text/css' href='/pve2/css/dd_style.css'>" $TEMPLATE_FILE); then
             echo "<link rel='stylesheet' type='text/css' href='/pve2/css/dd_style.css'>" >> $TEMPLATE_FILE
         fi 
@@ -184,7 +184,7 @@ function install {
         fi
 
         local IMAGELISTARR=($(echo "$IMAGELIST" | tr ',' '\n'))
-        if [ "$_silent" = false ]; then echo -e "Downloading images (0/${#IMAGELISTARR[@]})"; fi
+        if [ "$_silent" = false ]; then echo -e "Téléchargement des images (0/${#IMAGELISTARR[@]})"; fi
         ITER=0
         for image in "${IMAGELISTARR[@]}"
         do
@@ -194,34 +194,34 @@ function install {
                     cp "$OFFLINEDIR/PVEDiscordDark/images/$image" /usr/share/pve-manager/images/$image
                 fi
                 ((ITER++))
-                if [ "$_silent" = false ]; then echo -e "\e[1A\e[KDownloading images ($ITER/${#IMAGELISTARR[@]})"; fi
+                if [ "$_silent" = false ]; then echo -e "\e[1A\e[KTéléchargement des images ($ITER/${#IMAGELISTARR[@]})"; fi
         done
-        if [ "$_silent" = false ]; then echo -e "\e[1A\e[K${CHECKMARK} Downloading images (${#IMAGELISTARR[@]}/${#IMAGELISTARR[@]})"; fi
+        if [ "$_silent" = false ]; then echo -e "\e[1A\e[K${CHECKMARK} Téléchargement des images (${#IMAGELISTARR[@]}/${#IMAGELISTARR[@]})"; fi
 
-        if [ "$_silent" = false ]; then echo -e "Theme installed."; fi
+        if [ "$_silent" = false ]; then echo -e "Thème installé :-)."; fi
         if [ "$_noexit" = false ]; then exit 0; fi
     fi
 }
 
 function uninstall {
     if ! isInstalled; then
-        echo -e "${RED}Theme not installed${REG}"
+        echo -e "${RED}Theme non installé${REG}"
         exit 2
     else
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Removing stylesheet"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Suppression de la feuille de style"; fi
         rm /usr/share/pve-manager/css/dd_style.css
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Removing patcher"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Suppression du patcher"; fi
         rm /usr/share/pve-manager/js/dd_patcher.js
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Reverting changes to template file"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Retour en arriére sur les changements du fichier de template"; fi
         sed -i "/<link rel='stylesheet' type='text\/css' href='\/pve2\/css\/dd_style.css'>/d" /usr/share/pve-manager/index.html.tpl
         sed -i "/<script type='text\/javascript' src='\/pve2\/js\/dd_patcher.js'><\/script>/d" /usr/share/pve-manager/index.html.tpl
 
-        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Removing images"; fi
+        if [ "$_silent" = false ]; then echo -e "${CHECKMARK} Suppression des images"; fi
         rm /usr/share/pve-manager/images/dd_*
 
-        if [ "$_silent" = false ]; then echo -e "Theme uninstalled."; fi
+        if [ "$_silent" = false ]; then echo -e "Thème désintallé :-(."; fi
         if [ "$_noexit" = false ]; then exit 0; fi
     fi
 }
@@ -275,7 +275,7 @@ parse_cli()
                 fi
                 ;;
 	     *)
-				echo -e "${BRED}Error: Got an unexpected argument \"$_key\"${REG}\n"; 
+				echo -e "${BRED}Erreur: Erreur inattendue \"$_key\"${REG}\n"; 
                 usage;
                 exit 1;
 				;;
